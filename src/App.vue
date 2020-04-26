137c5c464e80741ff2c44631e3b3fe0b8009e990
tamueka <tamueka@hotmail.com>
2020-04-26 17:19:46 +0200

    layput nuevo app vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,89 @@
 <template>
-  <v-container>
-    <v-app id="app">
-      <v-layout>
-        <v-flex>
-          <Header></Header>
-        </v-flex>
-      </v-layout>
-
-      <v-layout>
-        <v-flex>
-          <router-view />
-        </v-flex>
-      </v-layout>
-    </v-app>
-  </v-container>
+<div id="app">
+  <v-app id="inspire">
+    <v-card class="overflow-hidden">
+      <v-app-bar
+        absolute
+        color="#6A76AB"
+        dark
+        shrink-on-scroll
+        prominent
+        src="https://picsum.photos/1920/1080?random"
+        fade-img-on-scroll
+        scroll-target="#scrolling-techniques-4"
+      >
+        <template v-slot:img="{ props }">
+          <v-img
+            v-bind="props"
+            gradient="to top right, rgba(100,115,201,.7), rgba(25,32,72,.7)"
+          ></v-img>
+        </template>
+  
+        <v-app-bar-nav-icon>
+          <v-icon>mdi-menu</v-icon>
+        </v-app-bar-nav-icon>
+  
+        <v-toolbar-title>Vuetify</v-toolbar-title>
+  
+        <v-spacer></v-spacer>
+  
+        <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>
+  
+        <v-btn icon>
+          <v-icon>mdi-heart</v-icon>
+        </v-btn>
+  
+        <v-menu
+          bottom
+          left
+        >
+          <template v-slot:activator="{ on }">
+            <v-btn
+              icon
+              color="yellow"
+              v-on="on"
+            >
+              <v-icon>mdi-dots-vertical</v-icon>
+            </v-btn>
+          </template>
+  
+          <v-list>
+            <v-list-item
+              v-for="(item, i) in items"
+              :key="i"
+            >
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </v-list-item>
+          </v-list>
+        </v-menu>
+  
+        <template v-slot:extension>
+          <v-tabs align-with-title>
+            <v-tab router-link to="/">Home</v-tab>
+            <v-tab router-link to="/botones">Botones</v-tab>
+            <v-tab router-link to="/usuarios">Usuarios</v-tab>
+            <v-tab router-link to="/about">About</v-tab>
+          </v-tabs>
+        </template>
+      </v-app-bar>
+      <v-sheet
+        id="scrolling-techniques-4"
+        class="overflow-y-auto"
+      >
+        <v-container style="height: 1000px; margin-top: 260px;">
+          <router-view></router-view>
+        </v-container>
+      </v-sheet>
+    </v-card>
+  </v-app>
+</div>
 </template>
 
 <script>
-import Header from "@/components/Header.vue";
-import Footer from "@/components/Footer.vue";
 
 export default {
   name: "App",
-  components: {
-    Header,
-    Footer
-  }
 };
 </script>
